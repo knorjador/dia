86777611dd8660eaf3aa96785edd0d48ca015c40
Asup45 <thibaut.dessolin@gmail.com>
2023-12-08 11:09:27 +0100

    back menu for all

--- filters.py
+++ filters.py
@@ -4,11 +4,12 @@ import pandas as pd
 import ast
 
 def menu_filters(username, df):
-    print('  Navigueur dans les films ----------- [1]')
-    print('  Filtrer par genre------------------- [2]')
-    print('  Filtrer par durée ------------------ [3]')
-    print('  Filtrer par pays ------------------- [4]')
-    print('  Retour menu principal -------------- [5]')
+    print('\n')
+    print('  Navigueur dans les films ----------------- [1]')
+    print('  Filtrer par genre------------------------- [2]')
+    print('  Filtrer par durée ------------------------ [3]')
+    print('  Filtrer par pays ------------------------- [4]')
+    print('  Retour menu principal -------------------- [5]')
     print('\n')
     set_choice(username, df, input('  Choix du menu : '))
 
@@ -16,20 +17,20 @@ def set_choice(username, df, choice):
     if(choice.isnumeric() and choice in ['1', '2', '3', '4','5']):
         match choice:
             case '1':
-                afficheFilm(df)
+                afficheFilm(username, df)
             case '2':
-                choisir_genre(df)
+                choisir_genre(username, df)
             case '3':
                 choisir_duree(username, df)
             case '4':
-                choisir_pays(df)
+                choisir_pays(username, df)
             case '5':
-                menu.display_menu(df)
+                menu.display_menu(username, df)
     else:
         print('\n Il faut choisir parmi les options proposées.\n')
         menu_filters(username, df)
 
-def afficheFilm(df):
+def afficheFilm(username, df):
     df_film = df['title']
     # Position initiale
     current_position = 0
@@ -42,7 +43,7 @@ def afficheFilm(df):
         print('')
 
         # Demander à l'utilisateur d'entrer "+", "-", ou "q"
-        user_input = input("Entrez '+' pour afficher les 5 suivants, '-' pour afficher les 5 précédents, ou 'q' pour quitter: ")
+        user_input = input("Entrez '+' pour afficher les 5 suivants, '-' pour afficher les 5 précédents, ou 'q' pour revenir au menu des filtres: ")
 
         if user_input == '+':
             current_position += 5
@@ -51,7 +52,7 @@ def afficheFilm(df):
             if current_position < 0:
                 current_position = 0
         elif user_input == 'q':
-            break
+            menu_filters(username, df)
         else:
             print("Entrée non valide. Veuillez entrer '+', '-', ou 'q'.")
 
@@ -67,18 +68,21 @@ def extract_names(row):
     
     return id_value, names
 
-def choisir_genre(df):
+def choisir_genre(username, df):
     df_genres = pd.DataFrame({
         'id': df['id'],
         'genres': df['genres']
     })
-    
+
+    # Convertir la colonne 'genre' en listes de dictionnaires
+    df_genres['genres'] = df_genres['genres'].apply(ast.literal_eval)
     # Appliquation de la fonction à chaque ligne du DataFrame
     result = df_genres.apply(extract_names, axis=1, result_type='expand')
     result.columns = ['id', 'genre_names']
 
     # On affiche le résultat
     print(result)
+    menu_filters(username, df)
 
 def choisir_duree(username, df):
     df_runtime = pd.DataFrame({
@@ -86,15 +90,15 @@ def choisir_duree(username, df):
         'runtime': df['runtime']
     })
 
-    print('  Entre 0h et 1h ---------------------------- [1]')
-    print('  Entre 1h et 1h30 -------------------------- [2]')
-    print('  Entre 1h30 et 2h -------------------------- [3]')
-    print('  2h et plus long --------------------------- [4]')
-    print('  Retour aux filtres ------------------------ [5]')
+    print('  Entre 0h et 1h --------------------------- [1]')
+    print('  Entre 1h et 1h30 ------------------------- [2]')
+    print('  Entre 1h30 et 2h ------------------------- [3]')
+    print('  2h et plus long -------------------------- [4]')
+    print('  Retour aux filtres ----------------------- [5]')
     print('\n')
     choice_runtime(df_runtime, input('  Choix de la durée : '), username, df)
 
-def choisir_pays(df):
+def choisir_pays(username, df):
     df_pays = pd.DataFrame({
     'id': df['id'],
     'pays': df['production_countries']
@@ -105,6 +109,7 @@ def choisir_pays(df):
     df_pays[['id', 'iso_3166_1']] = df_pays.apply(lambda row: pd.Series([row['id'], row['pays'][0]['iso_3166_1']] if row['pays'] and 'iso_3166_1' in row['pays'][0] else [row['id'], None]), axis=1)
     # On affiche le résultat
     print(df_pays[['id', 'iso_3166_1']])
+    menu_filters(username, df)
 
 
 
